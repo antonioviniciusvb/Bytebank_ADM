2e247f6afce6c40f4ce98f4a1958c22da88ba973
antonioviniciusvb <antonioviniciusvb@outlook.com>
2022-08-26 19:39:05 -0300

    Criação de classe Diretor e implementação da sobrecarga do métdo GetBonificao.

--- Bytebank_ADM/Funcionarios/Diretor.cs
+++ Bytebank_ADM/Funcionarios/Diretor.cs
@@ -8,7 +8,7 @@ namespace Bytebank_ADM.Funcionarios
 {
     public class Diretor : Funcionario
     {
-        public override double GetBonificacao()
+        public override  double GetBonificacao()
         {
             return Salario;
         }
