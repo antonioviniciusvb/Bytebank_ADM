e86893dd0dd1576928864b4c5df6e50e32fc4390
antonioviniciusvb <antonioviniciusvb@outlook.com>
2022-08-26 19:39:29 -0300

    Iniciando testes.

--- Bytebank_ADM/Program.cs
+++ Bytebank_ADM/Program.cs
@@ -1,13 +1,26 @@
-﻿using Bytebank_ADM.Funcionarios;
+﻿using bytebank;
+using Bytebank_ADM.Funcionarios;
 
 Console.WriteLine("Boas Vindas, ao ByteBank Administração.");
 
-Funcionario funcionario = new(tipo: 2)
+Funcionario funcionario = new()
 {
     Nome = "Vinicius",
     Cpf = "111.111.111-11",
     Salario = 6000
 };
 
-Console.WriteLine($"Bonificação: {funcionario.GetBonificacao():c}");
+Diretor diretor = new()
+{
+    Nome = "Bill Gates",
+    Cpf = "155.141.178-81",
+    Salario = 500000
+};
+
+Console.WriteLine($"Funcionário - Bonificação: {funcionario.GetBonificacao():c}");
+
+Util.PularLinhas(2);
+
+Console.WriteLine($"Diretor - Bonificação: {diretor.GetBonificacao():c}");
+
 Console.ReadKey();
