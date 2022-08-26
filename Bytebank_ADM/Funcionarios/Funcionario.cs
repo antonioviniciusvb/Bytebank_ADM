864882003e577794e79256499271782ad697c455
antonioviniciusvb <antonioviniciusvb@outlook.com>
2022-08-26 19:33:31 -0300

    Adicionar arquivos de projeto.

---
+++ Bytebank_ADM/Funcionarios/Funcionario.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bytebank_ADM.Funcionarios
+{
+    public class Funcionario
+    {
+
+        //public Funcionario(int tipo)
+        //{
+        //    Tipo = tipo;
+        //}
+
+        // 0 - funcionario
+        // 1 - diretor
+        // 2 - designer
+        //public int Tipo { get; private set; }
+        public string Nome { get; set; }
+
+        public string Cpf { get; set; }
+
+        public double Salario { get; set; }
+
+        /// <summary>
+        /// Método para obter a Bonificação do Funcionario
+        /// </summary>
+        /// <returns></returns>
+        public virtual double GetBonificacao()
+        {
+            return Salario * 0.1;
+        }
+    }
+}
---
+++ Bytebank_ADM/Program.cs
@@ -0,0 +1,13 @@
+﻿using Bytebank_ADM.Funcionarios;
+
+Console.WriteLine("Boas Vindas, ao ByteBank Administração.");
+
+Funcionario funcionario = new(tipo: 2)
+{
+    Nome = "Vinicius",
+    Cpf = "111.111.111-11",
+    Salario = 6000
+};
+
+Console.WriteLine($"Bonificação: {funcionario.GetBonificacao():c}");
+Console.ReadKey();
