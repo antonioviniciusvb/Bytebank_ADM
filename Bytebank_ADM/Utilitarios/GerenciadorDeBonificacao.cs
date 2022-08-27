676c455e0a94295378239f53161b61d5e6511faa
antonioviniciusvb <antonioviniciusvb@outlook.com>
2022-08-26 21:06:58 -0300

    Implementação da classe de gerenciamento.

---
+++ Bytebank_ADM/Utilitarios/GerenciadorDeBonificacao.cs
@@ -0,0 +1,24 @@
+﻿using Bytebank_ADM.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bytebank_ADM.Utilitarios
+{
+    public class GerenciadorDeBonificacao
+    {
+        private double _totalBonificacao;
+
+        public void Registrar(Funcionario funcionario)
+        {
+            _totalBonificacao += funcionario.GetBonificacao();
+        }
+
+        public double GetBonificacao()
+        {
+            return _totalBonificacao;
+        }
+    }
+}
